f4e7aa83761a2e786b91859921ec7aab6dc0e313
Dancan1907 <dancankalerwa@gmail.com>
2026-02-12 21:10:33 +0300

    create multiple pages using react-router-dom

--- commas-project/src/App.jsx
+++ commas-project/src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index  element={<HomePage />} />
+      <Route path="/orders" element={<div>Orders Page</div>} />
+      <Route path="/checkout" element={<div>Checkout Page</div>} />
+      {/* Add more routes here as needed */}
+    </Routes>
+      
+  
   )
 }
 
