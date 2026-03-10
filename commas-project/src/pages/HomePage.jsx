2fde54c2eeeebf010c285b1a8ac3327fe60633c1
Dancan1907 <dancankalerwa@gmail.com>
2026-03-10 05:31:10 +0300

    minor change on homepage

--- commas-project/src/pages/HomePage.jsx
+++ commas-project/src/pages/HomePage.jsx
@@ -84,7 +84,7 @@ export function HomePage() {
             <div className="product-quantity-container">
               <select>
                 <option value="1">1</option>
-                <option value="2">2</option>
+                <option value="2">2</option>  
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
